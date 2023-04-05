c923719131ead61e80707a8e4c6f904e3f3f52a7
Boyan Iliev <97385003+Boyan253@users.noreply.github.com>
2023-04-05 12:43:44 +0300

    localhost socket io update

--- Framework/index.js
+++ Framework/index.js
@@ -49,7 +49,7 @@ async function start() {
 
     const socketIO = require('socket.io')(http, {
         cors: {
-            origin: "mongodb+srv://bobosa2534:Bandarara0506@cluster0.lh5vqlc.mongodb.net/?retryWrites=true&w=majority"
+            origin: "http://localhost:3000"
         }
     });
 
