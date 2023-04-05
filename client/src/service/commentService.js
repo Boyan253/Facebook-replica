ae5a1a53618d78184251b0292926173993feea8c
Boyan Iliev <97385003+Boyan253@users.noreply.github.com>
2023-04-05 13:10:08 +0300

    Update commentService.js

--- client/src/service/commentService.js
+++ client/src/service/commentService.js
@@ -1,5 +1,5 @@
 import * as  request from "./requester/requester";
-let baseUrl = 'http://localhost:3005/posts/'
+let baseUrl = 'www.thefuture.com/posts/'
 
 
 export async function addComment(postId, data) {
