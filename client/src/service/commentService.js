4a6ff2559de34b51e2c396b02fc2fee5becf992b
Boyan Iliev <97385003+Boyan253@users.noreply.github.com>
2023-04-28 17:41:01 +0300

    Update commentService.js

--- client/src/service/commentService.js
+++ client/src/service/commentService.js
@@ -4,13 +4,13 @@ let baseUrl = 'https://future-server.onrender.com/posts/'
 
 export async function addComment(postId, data) {
     const result = await request.post(baseUrl + postId + '/comments', data)
-    console.log(result);
+   
 }
 
 export const getComments = async (postId) => {
     try {
         const response = await fetch(baseUrl + postId + '/comments');
-        console.log(response);
+      
         const data = await response.json();
         return data;
     } catch (error) {
