030b787b8cb0d8f68cdfc76761233e48687419cb
Boyan Iliev <97385003+Boyan253@users.noreply.github.com>
2023-05-11 22:33:51 +0300

    Update Sidebar.js

--- client/src/components/sidebar/Sidebar.js
+++ client/src/components/sidebar/Sidebar.js
@@ -2,13 +2,8 @@ import "./sidebar.css";
 import {
   RssFeed,
   Chat,
-  // PlayCircleFilledOutlined,
-  // Group,
-  // Bookmark,
-  // HelpOutline,
-  // WorkOutline,
+  Group,
   Event,
-  // School,
 } from "@material-ui/icons";
 import CloseFriend from "../closeFriend/CloseFriend";
 import { useContext, useEffect, useState } from "react";
@@ -16,31 +11,29 @@ import { AuthContext } from "../../contexts/AuthContext";
 import { Link } from "react-router-dom";
 
 export default function Sidebar({ user }) {
-  const { auth } = useContext(AuthContext)
-  // const [user, setUser] = useState()
+  const { auth } = useContext(AuthContext);
+  const [showMore, setShowMore] = useState(false);
+  const [friends, setFriends] = useState([]);
+
   if (auth?._id) {
-
-    user = auth
-    user.id = auth._id
-
+    user = auth;
+    user.id = auth._id;
   }
 
   useEffect(() => {
     const getFriends = async () => {
       try {
         if (user.id) {
-          const response = await fetch(`https://future-server.onrender.com/friends/${user.id}`)
-          const friendList = await response.json()
-          setFriends(friendList)
+          const response = await fetch(`http://localhost:3005/friends/${user.id}`);
+          const friendList = await response.json();
+          setFriends(friendList);
         }
       } catch (err) {
-        console.log(err)
+        console.log(err);
       }
-    }
-    getFriends()
-
-  }, [user?.id])
-  const [friends, setFriends] = useState([])
+    };
+    getFriends();
+  }, [user?.id]);
 
   return (
     <div className="sidebar">
@@ -50,38 +43,38 @@ export default function Sidebar({ user }) {
             <RssFeed className="sidebarIcon" />
             <Link to={'/posts'}> <span className="sidebarListItemText">Posts</span></Link>
           </li>
-          {auth?._id ? <li className="sidebarListItem">
-            <Chat className="sidebarIcon" />
-            <Link to={'/chat'}><span className="sidebarListItemText">Chat</span></Link>
-          </li> : ''}
-
-          {/* <li className="sidebarListItem">
-            <PlayCircleFilledOutlined className="sidebarIcon" />
-            <span className="sidebarListItemText">Videos</span>
-          // </li>
-          // // <li className="sidebarListItem">
-          // //   <Group className="sidebarIcon" />
-          // //   <span className="sidebarListItemText">Groups</span>
-          // // </li>
-          <li className="sidebarListItem">
-            <Bookmark className="sidebarIcon" />
-            <span className="sidebarListItemText">Bookmarks</span>
-          </li>
-          <li className="sidebarListItem">
-            <HelpOutline className="sidebarIcon" />
-            <span className="sidebarListItemText">Questions</span>
-          </li>
-          <li className="sidebarListItem">
-            <WorkOutline className="sidebarIcon" />
-            <span className="sidebarListItemText">Jobs</span>
-          </li> */}
-          <li className="sidebarListItem">
-            <Event className="sidebarIcon" />
-            <span className="sidebarListItemText">Contact Us</span>
-          </li>
-           
+          {auth?._id ? (
+            <>
+              <li className="sidebarListItem">
+                <Chat className="sidebarIcon" />
+                <Link to={'/chat'}><span className="sidebarListItemText">Chat</span></Link>
+              </li>
+              <li className="sidebarListItem">
+                <Group className="sidebarIcon" />
+                <Link to={`/profile/${auth._id}`}> <span className="sidebarListItemText">My Profile</span></Link>
+              </li>
+              {showMore && (
+                <>
+                  <li className="sidebarListItem">
+                    <Event className="sidebarIcon" />
+                    <span className="sidebarListItemText">Contact Us</span>
+                  </li>
+                </>
+              )}
+            </>
+          ) : <p style={{ color: 'red' }}>login to see more</p>}
         </ul>
-        <button className="sidebarButton">More</button>
+        <button className="sidebarButton" onClick={(e) => {
+          let btn = e.target
+          if (btn.textContent == 'More') {
+            btn.textContent = 'Less'
+
+          } else {
+            btn.textContent = 'More'
+
+          }
+          setShowMore(!showMore);
+        }}>More</button>
         <hr className="sidebarHr" />
         <ul className="sidebarFriendList">
           {friends.length > 0 ? friends.map((friend) => (
@@ -89,6 +82,6 @@ export default function Sidebar({ user }) {
           )) : <p>No friends yet</p>}
         </ul>
       </div>
-    </div>
+    </div >
   );
 }
